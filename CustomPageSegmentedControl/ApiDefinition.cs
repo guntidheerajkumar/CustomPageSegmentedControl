4791d96b5ffd570f25c6d32bd3ee723e3246f9f2
Dheeraj Kumar G <guntidheerajkumar@gmail.com>
2016-12-06 15:52:46 +0530

    Custom Page Segmented Control
    Custom Page Segmented Control

---
+++ CustomPageSegmentedControl/ApiDefinition.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Foundation;
+using ObjCRuntime;
+using UIKit;
+
+namespace CustomPageSegmentedControl
+{
+	// @protocol ARSegmentControllerDelegate <NSObject>
+	[Protocol, Model]
+	[BaseType(typeof(NSObject))]
+	interface ARSegmentControllerDelegate
+	{
+		// @required -(NSString *)segmentTitle;
+		[Abstract]
+		[Export("segmentTitle")]
+		string SegmentTitle { get; }
+
+		// @optional -(id)streachScrollView;
+		[Export("streachScrollView")]
+		NSObject StreachScrollView { get; }
+	}
+
+	// @protocol ARSegmentPageControllerHeaderProtocol <NSObject>
+	[Protocol, Model]
+	[BaseType(typeof(NSObject))]
+	interface ARSegmentPageControllerHeaderProtocol
+	{
+		// @required @property (nonatomic) UIImageView * imageView;
+		[Abstract]
+		[Export("imageView", ArgumentSemantic.Assign)]
+		UIImageView ImageView { get; set; }
+	}
+
+	// @interface ARSegmentPageHeader : UIView <ARSegmentPageControllerHeaderProtocol>
+	[BaseType(typeof(UIView))]
+	interface ARSegmentPageHeader : ARSegmentPageControllerHeaderProtocol
+	{
+		// @property (nonatomic, strong) UIImageView * imageView;
+		[Export("imageView", ArgumentSemantic.Strong)]
+		UIImageView ImageView { get; set; }
+	}
+
+	// @interface ARSegmentPageController : UIViewController
+	[BaseType(typeof(UIViewController))]
+	interface ARSegmentPageController
+	{
+		// @property (assign, nonatomic) CGFloat segmentHeight;
+		[Export("segmentHeight")]
+		nfloat SegmentHeight { get; set; }
+
+		// @property (assign, nonatomic) CGFloat headerHeight;
+		[Export("headerHeight")]
+		nfloat HeaderHeight { get; set; }
+
+		// @property (assign, nonatomic) CGFloat segmentMiniTopInset;
+		[Export("segmentMiniTopInset")]
+		nfloat SegmentMiniTopInset { get; set; }
+
+		// @property (readonly, assign, nonatomic) CGFloat segmentToInset;
+		[Export("segmentToInset")]
+		nfloat SegmentToInset { get; }
+
+		// @property (readonly, nonatomic, weak) UIViewController<ARSegmentControllerDelegate> * currentDisplayController;
+		[Export("currentDisplayController", ArgumentSemantic.Weak)]
+		ARSegmentControllerDelegate CurrentDisplayController { get; }
+
+		// @property (readonly, nonatomic, strong) UIView<ARSegmentPageControllerHeaderProtocol> * headerView;
+		[Export("headerView", ArgumentSemantic.Strong)]
+		ARSegmentPageHeader HeaderView { get; }
+
+		// @property (assign, nonatomic) BOOL freezenHeaderWhenReachMaxHeaderHeight;
+		[Export("freezenHeaderWhenReachMaxHeaderHeight")]
+		bool FreezenHeaderWhenReachMaxHeaderHeight { get; set; }
+
+		// -(instancetype)initWithControllers:(UIViewController<ARSegmentControllerDelegate> *)controller, ... __attribute__((sentinel(0, 1)));
+		[Internal]
+		[Export("initWithControllers:", IsVariadic = true)]
+		IntPtr Constructor(ARSegmentControllerDelegate controller, IntPtr varArgs);
+
+		// -(void)setViewControllers:(NSArray *)viewControllers;
+		[Export("setViewControllers:")]
+		void SetViewControllers(NSObject[] viewControllers);
+
+		// -(UIView<ARSegmentPageControllerHeaderProtocol> *)customHeaderView;
+		[Export("customHeaderView")]
+		ARSegmentPageControllerHeaderProtocol CustomHeaderView { get; }
+	}
+
+	// @interface ARSegmentView : UIToolbar
+	[BaseType(typeof(UIToolbar))]
+	interface ARSegmentView
+	{
+		// @property (readonly, nonatomic, strong) UISegmentedControl * segmentControl;
+		[Export("segmentControl", ArgumentSemantic.Strong)]
+		UISegmentedControl SegmentControl { get; }
+	}
+}
---
+++ Sample/ViewController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using CustomPageSegmentedControl;
+using Foundation;
+using CoreGraphics;
+using UIKit;
+
+namespace Sample
+{
+	public partial class ViewController : UIViewController, IARSegmentControllerDelegate
+	{
+		UIImage defaultImage;
+		UIImage blurImage;
+		ARSegmentPageController pager;
+
+		public string SegmentTitle {
+			get {
+				return "Sample";
+			}
+		}
+
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+
+			this.defaultImage = UIImage.FromFile("background.jpg");
+			this.blurImage = UIImage.FromFile("background.jpg");
+
+			BtnClickMe.TouchUpInside += (sender, e) => {
+				if (pager != null) {
+					pager.RemoveObserver(this, new NSString("segmentTopInset"));
+					pager = null;
+				}
+
+				var arPager = new ARSegmentPageController();
+				var c = UIStoryboard.FromName("Main", null).InstantiateViewController("TableViewController") as TableViewController;
+				arPager.SetViewControllers(new NSObject[] { c });
+				arPager.SegmentMiniTopInset = 64f;
+				arPager.HeaderHeight = 200f;
+				arPager.SegmentHeight = 0f;
+				arPager.FreezenHeaderWhenReachMaxHeaderHeight = true;
+				this.pager = arPager;
+				this.pager.AddObserver(this, new NSString("segmentTopInset"), NSKeyValueObservingOptions.New, IntPtr.Zero);
+				this.NavigationController.PushViewController(this.pager, true);
+			};
+		}
+
+		public override void ObserveValue(NSString keyPath, NSObject ofObject, NSDictionary change, IntPtr context)
+		{
+			var topInset = ((NSNumber)change.ValueForKey(ChangeNewKey)).NFloatValue;
+			var imageView = this.pager.HeaderView.ImageView;
+			if (topInset <= this.pager.SegmentMiniTopInset) {
+				this.pager.Title = "Page Title";
+				imageView.Image = this.blurImage;
+			} else {
+				this.pager.Title = string.Empty;
+				imageView.Image = this.defaultImage;
+			}
+		}
+
+		public override UIStatusBarStyle PreferredStatusBarStyle()
+		{
+			return UIStatusBarStyle.LightContent;
+		}
+
+		public class PageHeader : ARSegmentPageHeader
+		{
+			public override UIImageView ImageView {
+				get {
+					return base.ImageView;
+				}
+				set {
+					base.ImageView = value;
+				}
+			}
+		}
+	}
+}
